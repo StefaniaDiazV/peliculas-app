40ea90753dc960fd409636ab96921f3cd5115b18
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-01-30 17:31:16 -0500

    Update/Armando layout de las paginas

---
+++ src/pages/New-releases/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layout"
+
+const NewReleases = () => {
+    return (
+        <Layout>
+            Nuevos lanzamientos
+        </Layout>
+    )
+}
+
+export { NewReleases }
---
+++ src/pages/Top/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layout"
+
+const Top = () => {
+    return (
+        <Layout>
+            Top movies
+        </Layout>
+    )
+}
+
+export { Top }
