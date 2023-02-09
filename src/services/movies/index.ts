0eaac655ad0f4686a80d84027425f12b20df0300
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-09 16:41:45 -0500

    Update/Agregando mapToArray

--- src/services/movies/index.ts
+++ src/services/movies/index.ts
@@ -1,9 +1,10 @@
-
+import { mapToArray } from "../../helpers/mapToArray"
+import { Movie } from "../../types"
 import { apiMovies } from "../../utils/axios"
 
 const get = async (params:string) => {
   const response =  await apiMovies.get(params)
-  return response.data.results
+  return mapToArray<Movie>(response.data.results)
 }
 
 export const moviesServices = { get }
