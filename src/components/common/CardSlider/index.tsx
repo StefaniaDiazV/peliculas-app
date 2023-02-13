65c2f93c122bf55cc9381ed9d62f2209821ed933
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-13 16:24:07 -0500

    Update/Agregando breakpoints al CardSlider

--- src/components/common/CardSlider/index.tsx
+++ src/components/common/CardSlider/index.tsx
@@ -6,40 +6,44 @@ import { FC, useEffect, useState } from "react";
 import { Movie } from "../../../types";
 import { moviesServices } from "../../../services/movies";
 import { CardMovie } from "../CardMovie";
-import { base_url, poster_sizes } from "../../../constants";
+import { base_url, breakpointsSlider, poster_sizes } from "../../../constants";
 
 type Props = {
-    search: string
-    listTitle: string
-}
+  search: string;
+  listTitle: string;
+};
 
-const CardSlider: FC<Props> = ({search, listTitle}) => {
-  
-    const [ movies, setMovies ] = useState<Movie[]>([])
+const CardSlider: FC<Props> = ({ search, listTitle }) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    useEffect(() => {
-        moviesServices.get(search).then((data) => setMovies(data))
-    }, [])
+  useEffect(() => {
+    moviesServices.get(search).then((data) => setMovies(data));
+  }, []);
 
-    return (
+  return (
     <div className="py-4 px-4 justify-content-center">
-        <h2>{listTitle}</h2>
+      <h2>{listTitle}</h2>
       <Swiper
         modules={[Navigation, Pagination, A11y]}
-        spaceBetween={30}
-        slidesPerView={5}
         navigation
         pagination={{ clickable: true }}
         loop={true}
+        breakpoints={breakpointsSlider}
       >
-        {movies && movies.filter((v, index) => index < 11).map((movie) => 
-             <SwiperSlide key={movie.id}>
-                <CardMovie title={movie.title} img={`${base_url}${poster_sizes[3]}${movie.poster_path}`}/>
-             </SwiperSlide>
-        )}
+        {movies &&
+          movies
+            .filter((v, index) => index < 11)
+            .map((movie) => (
+              <SwiperSlide key={movie.id}>
+                <CardMovie
+                  title={movie.title}
+                  img={`${base_url}${poster_sizes[3]}${movie.poster_path}`}
+                />
+              </SwiperSlide>
+            ))}
       </Swiper>
     </div>
   );
 };
 
-export { CardSlider }
+export { CardSlider };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,36 @@
-const base_url = "http://image.tmdb.org/t/p/"
-const poster_sizes = ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original']
-const backdrop_sizes = ['w300', 'w780', 'w1280', 'original']
+const base_url = "http://image.tmdb.org/t/p/";
+const poster_sizes = [
+  "w92",
+  "w154",
+  "w185",
+  "w342",
+  "w500",
+  "w780",
+  "original",
+];
+const backdrop_sizes = ["w300", "w780", "w1280", "original"];
 
+const breakpointsSlider = {
+  0: {
+    slidesPerView: 1,
+    spaceBetween: 10,
+  },
+  480: {
+    slidesPerView: 2,
+    spaceBetween: 10,
+  },
+  768: {
+    slidesPerView: 3,
+    spaceBetween: 15,
+  },
+  1024: {
+    slidesPerView: 4,
+    spaceBetween: 15,
+  },
+  1280: {
+    slidesPerView: 5,
+    spaceBetween: 30,
+  },
+};
 
-export { poster_sizes, base_url, backdrop_sizes }
+export { poster_sizes, base_url, backdrop_sizes, breakpointsSlider };
