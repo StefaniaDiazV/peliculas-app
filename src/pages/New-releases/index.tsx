9cec23a6fd74a9f4b0fdbe5712a863bd66a59f8a
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-13 16:52:31 -0500

    Update/Pintando los ultimos lanzamientos

--- src/pages/New-releases/index.tsx
+++ src/pages/New-releases/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+import { CardMovie } from "../../components/common/CardMovie"
 import { Layout } from "../../components/layout"
+import { base_url, poster_sizes } from "../../constants"
+import { moviesServices } from "../../services/movies"
+import { Movie } from "../../types"
 
 const NewReleases = () => {
+
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    useEffect(() => {
+        moviesServices.get("upcoming").then((data) => setMovies(data));
+      }, []);
+
     return (
         <Layout>
-            Nuevos lanzamientos
+            <Container fluid className="p-4">
+        <h2>Ultimos lanzamientos</h2>
+        <Row>
+          {movies &&
+            movies.map((movie) => (
+              <Col key={movie.id} sm={6} md={4} lg={3} className="mb-4">
+                <CardMovie
+                  title={movie.title}
+                  img={`${base_url}${poster_sizes[3]}${movie.poster_path}`}
+                />
+              </Col>
+            ))}
+        </Row>
+      </Container>
         </Layout>
     )
 }
