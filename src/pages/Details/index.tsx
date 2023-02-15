8018b2bcd12f8fc5ea77456d561b9f08db6757cd
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-15 09:37:03 -0500

    Update/Estilos details-page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,11 +1,40 @@
-import { Layout } from "../../components/layout"
+import { Container } from "react-bootstrap";
+import { Layout } from "../../components/layout";
+import "./style.scss";
 
 const Details = () => {
-    return(
-        <Layout>
+  return (
+    <Layout>
+      <Container className="details" fluid>
+        <div className="box-text">
+          <h2>Titulo</h2>
+          <div className="detail-top">
+            {/* //<StarFill/> */}
+            <span className="votes">35</span>
+            <span className="year"> 2022</span>
+            <span className="country">Corea</span>
+          </div>
+          <p>
+            Darcy and Tom gather their families for the ultimate destination
+            wedding but when the entire party is taken hostage, “’Til Death Do
+            Us Part” takes on a whole new meaning in this hilarious,
+            adrenaline-fueled adventure as Darcy and Tom must save their loved
+            ones—if they don’t kill each other first.
+          </p>
+          <div className="details-bottom">
+            <p>Generos : <span>Accion , comedia </span></p>
+            <p>Productoras: <span> Warner</span>
+            </p>
+          </div>
+        </div>
+        <div className="img-box">
+          <img
+            src={`https://image.tmdb.org/t/p/w1000_and_h450_multi_faceshttps://www.themoviedb.org/t/p/original/ur7Z7E6t1B6fWMreXNMpsUTcBGX.jpg`}
+          />
+        </div>
+      </Container>
+    </Layout>
+  );
+};
 
-        </Layout>
-    )
-}
-
-export { Details }
+export { Details };
