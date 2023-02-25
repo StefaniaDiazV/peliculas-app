eb1148b07988b07410b8e856bd55daf17706f3ee
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-24 20:32:01 -0500

    Update/Creando funciones getAll, getBy y update en userServices

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,4 +1,5 @@
-import { SignUpPayload } from "../../types";
+import { mapToArray } from "../../helpers/mapToArray";
+import { SignUpPayload, User } from "../../types";
 import { api } from "../../utils/axios";
 
 const add = async (user: SignUpPayload) => {
@@ -6,5 +7,19 @@ const add = async (user: SignUpPayload) => {
     return response.data;
   };
 
+const getAll = async (): Promise<User[]> => {
+    const response = await api.get("/users.json");
+    return mapToArray(response.data);
+};
 
-  export const userServices =  { add }
+const getBy = async (type: "email" | "token", value: string) => {
+    const users = await getAll();
+    const user = users.find((user) => user[type] === value);
+    return user;
+};
+  
+const update = ({ id, ...rest }: Partial<User>) => {
+    const response = api.patch(`/users/${id}.json`, { ...rest });
+};
+
+  export const userServices =  { add, getAll, getBy, update }
