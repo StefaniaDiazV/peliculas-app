33bc29e5a0f49a0a84d24afc2daf5039f3597aac
Stefania Diaz <stefaniadiazvalencia@gmail.com>
2023-02-27 16:56:21 -0500

    Update/Funcionalidad loginForm

--- src/components/forms/Login/index.tsx
+++ src/components/forms/Login/index.tsx
@@ -1,26 +1,36 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import './style.scss'
+import { useForm } from "react-hook-form";
+import { useMe } from "../../../hooks";
+import { loginForm } from "../../../types";
+import "./style.scss";
 
 const Login = () => {
-    return (
-        <Form className='login-form bg-dark' >
+  const { register, handleSubmit } = useForm<loginForm>();
 
+  const { login } = useMe();
+
+  const onSubmit = (data: loginForm) => {
+   login(data)
+  };
+
+  return (
+    <Form className="login-form bg-dark" onSubmit={handleSubmit(onSubmit)}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Ingresar email" />
+        <Form.Control type="email" placeholder="Ingresar email"  {...register("email")} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Ingresar contraseña" />
+        <Form.Control type="password" placeholder="Ingresar contraseña" {...register("pass")} />
       </Form.Group>
 
       <Button variant="primary" type="submit">
         Ingresar
       </Button>
     </Form>
-    )
-}
+  );
+};
 
-export { Login }
+export { Login };
